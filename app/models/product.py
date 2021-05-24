ce0f18505997d8eb7cca2d64976110bdb1dfbd3e
Rafał <rafalp443@gmail.com>
2021-05-24 18:11:47 +0200

    2021-05-24 RP

--- app/models/product.py
+++ app/models/product.py
@@ -1,4 +1,4 @@
-import request
+import requests
 import json
 from bs4 import BeautifulSoup
 from app.utils import extractComponent
@@ -19,7 +19,7 @@ class Product:
             opinions = pageDOM.select("div.js_product-review")
 
             for opinion in opinions:
-                self.opinions.append(Opinion().extractOpinion(opinion))
+                self.opinions.append(Opinion().extractOpinion(opinion).transformOpinion())
 
             response = requests.get("https://www.ceneo.pl/{}/opinie-".format(self.productId) + str(page), allow_redirects=False)
             if response.status_code == 200:
@@ -28,11 +28,18 @@ class Product:
                 break
     
     def exportProduct(self):
-        with open("./opinions/{}.json".format(self.productId), "w", encoding="UTF-8") as f:
-            json.dump(self.opinions, f, indent=4, ensure_ascii=False)
+        with open("app/products/{}.json".format(self.productId), "w", encoding="UTF-8") as f:
+            json.dump(self.toDict(), f, indent=4, ensure_ascii=False)
     
-    def __dict__(self):
-        pass
+    def toDict(self):
+        return {
+            "productId": self.productId,
+            "productName": self.productName,
+            "opinions": [opinion.toDict() for opinion in self.opinions]
+        }
 
     def __str__(self) -> str:
-        pass
+        return f"productId: {self.productId}<br>productName: {self.productName}<br>opinions<br><br>" + "<br><br>".join(str(opinion) for opinion in self.opinions)
+
+    def __repr__(self) -> str:
+        return f"Product(productId={self.productId}, productName={self.productName}, opinions=[" + ", ".join(opinion.__repr__() for opinion in self.opinions) + "])"
